5b3c6ef8fdecec14bf31a37ae50cbcb5f5d24b06
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-01-29 16:48:10 -0500

    Deleted all article posts not associated with a Category

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
-  has_many :categories, dependent: :destroy
+  belongs_to :category
   validates :title, presence: true, length: {minimum: 2}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129202945) do
+ActiveRecord::Schema.define(version: 20150129211631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,18 +19,17 @@ ActiveRecord::Schema.define(version: 20150129202945) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "name"
+    t.integer  "category_id"
   end
+
+  add_index "articles", ["category_id"], name: "index_articles_on_category_id", using: :btree
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name"
   end
-
-  add_index "categories", ["article_id"], name: "index_categories_on_article_id", using: :btree
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -42,6 +41,6 @@ ActiveRecord::Schema.define(version: 20150129202945) do
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
-  add_foreign_key "categories", "articles"
+  add_foreign_key "articles", "categories"
   add_foreign_key "comments", "articles"
 end
