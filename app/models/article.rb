9170b5d59c274a95b938509fca6fd1a51b79b8c6
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-01-29 15:52:54 -0500

    Add Categories model, association & records

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  has_many :categories, dependent: :destroy
   validates :title, presence: true, length: {minimum: 2}
 end
